e9c6eabc3b3f86f946f677efb329bfc910d2f811
fahmy-dev <faahmy@proton.me>
2024-11-27 12:43:18 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(office = 'go to the office') {
+    return `This Monday, I will ${office}.`
+}
+mondayWork();
+
+function wrapAdjective(wrap = "*") {
+    const inner = function(word = 'special') {
+        return `You are ${wrap}${word}${wrap}!`
+    };
+    return inner;
+}
+    wrapAdjective();
